bbe1ba478b9c9ec86b864f2c4388b802b092470b
Noise859 <brennan.neuser@gmail.com>
2017-08-11 23:04:25 -0500

    lmao

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 var nameOverlay = document.getElementById("chooseNameOverlay"),
-    usernameInput = document.getElementById("username"),
     gameElement = document.getElementById("body"),
     money = 500,
     name;
-    
+
 function start() {
   console.log("Lmao");
   nameOverlay.style.display = "none"; 
-  gameElement.style.overflow = "auto";
-  name = usernameInput.value;
+  name = $('#username').val();
 }
 
 function getRandomInt(a,b) {
